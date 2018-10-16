90517997f3f92818ddc0f99bcb82bd0367e831c1
Jack Lam <jlam00@stuy.edu>
2018-10-16 11:02:06 -0400

    add mr. k stuff

--- RealNumber.java
+++ RealNumber.java
@@ -1,13 +1,41 @@
-public class RealNumber {
-  private double num;
-  public RealNumber(){
-    num = 1;
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
   }
-  public RealNumber(double value){
-    num = value;
+
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+    return this.getValue + other.getValue;
   }
-  public void getNum(){}
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return this.getValue * other.getValue;;
+  }
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return this.getValue / other.getValue;;
+  }
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return this.getValue - other.getValue;
+  }
+
+
+  public double getValue(){
+    return value;
+  }
+
   public String toString(){
-    return null;
+    return ""+ value;
   }
 }
