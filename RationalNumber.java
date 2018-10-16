96fadb0a76107f3ac5d288959435012d3c190537
Jack Lam <jlam00@stuy.edu>
2018-10-15 22:06:52 -0400

    empty methods

---
+++ RationalNumber.java
@@ -0,0 +1,15 @@
+public class RationalNumber extends RealNumber{
+  private RealNumber numerator;
+  private RealNumber denominator;
+  public RationalNumber(){
+    super();}
+  public RationalNumber(RealNumber nume, RealNumber deno){
+    numerator = nume;
+    denominator = deno;
+  }
+  public void getRaNum(){
+  }
+  public void Compare(RealNumber real, RationalNumber Rational){}
+  public void CompareTo(RationalNumber Rational1, RationalNumber Raitonal2){}
+
+  }
---
+++ RealNumber.java
@@ -0,0 +1,13 @@
+public class RealNumber {
+  private double num;
+  public RealNumber(){
+    num = 1;
+  }
+  public RealNumber(double value){
+    num = value;
+  }
+  public void getNum(){}
+  public String toString(){
+    return null;
+  }
+}
